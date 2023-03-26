729428776f43ceea70b3e15dacaaa42467a6ae77
Daniel Slade <daniel@slade.net.nz>
2023-03-27 09:31:15 +1300

    [new] fizzbuzz does not accept numbers less than 1

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,7 @@
 export function fizzbuzz(number: number): string {
+  if (number < 1) {
+    throw new Error("Too small");
+  }
+
   return "";
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -8,4 +8,8 @@ describe("fizzbuzz", () => {
   it("should accept a number and return a string", () => {
     expect(typeof fizzbuzz(4)).toBe("string");
   });
+
+  it("should not expect numbers less than 1", () => {
+    expect(() => fizzbuzz(0)).toThrow("Too small");
+  });
 });
