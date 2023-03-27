596ad08dcf12a21a369213effe05bc4413139a9a
Daniel Slade <daniel@slade.net.nz>
2023-03-27 17:04:47 +1300

    [refactor] add return type to password validator

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,4 +1,11 @@
-export function validatePassword(password: string) {
+type PasswordValidationError = "PasswordTooShort" | "PasswordTooLong";
+
+export type PasswordValidationResult = {
+  valid: boolean;
+  errors: PasswordValidationError[];
+};
+
+export function validatePassword(password: string): PasswordValidationResult {
   if (password.length < 5) {
     return {
       valid: false,
